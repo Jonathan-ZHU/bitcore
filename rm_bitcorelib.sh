5fd7d26a0edad3972937c6eb73dcc5bbf33d9572
Jonathan <839560084@qq.com>
2017-07-14 17:13:17 +0800

    for mbc with all modules

--- rm_bitcorelib.sh
+++ rm_bitcorelib.sh
@@ -3,4 +3,4 @@
 rm -r ./node_modules/bitcore-node/node_modules/bitcore-lib && \
 #rm -r ./node_modules/bitcore-lib && \
 rm -r ./node_modules/insight-api/node_modules/bitcore-lib
-
+rm -r ./node_modules/bitcore-message/node_modules/bitcore-lib
